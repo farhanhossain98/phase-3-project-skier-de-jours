10c270d24a6ba4c1b4343d6467f7d5c40adc6f95
farhan hossain <farhanrufon99@gmail.com>
2023-09-05 11:40:54 -0400

    adds debug file

--- lib/classes/event.py
+++ lib/classes/event.py
@@ -12,4 +12,23 @@ class Event:
         return self._capacity
     
     @capacity.setter
-    def capacity(self, name)
+    def capacity(self, name):
+        pass
+
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, name):
+        pass
+
+# return a list events happening and requirements needed to enter
+# return a list of participants 
+
+
+
+# 
+    # from classes.registration import Registration
+    # from classes.riding_team import RidingTeam
+    # from classes.skier import Skier
--- lib/classes/riding_team.py
+++ lib/classes/riding_team.py
@@ -2,7 +2,7 @@ class RidingTeam:
 
     all = []
 
-    def __init__(self,horse_name, rider_name, equipment):
+    def __init__(self,horse_name, rider_name):
         self.horse_name = horse_name
         self.rider_name = rider_name
         RidingTeam.all.append(self)
@@ -14,17 +14,43 @@ class RidingTeam:
 
     @horse_name.setter
     def horse_name(self, name):
-        pass
+        if type(name) is str:
+            if len(name) > 0:
+                self._horse_name = name
+            else:
+                raise ValueError('Name must be more than 0 characters long.')
+        else:
+            TypeError('Name must be a string.')
+        
 
     @property
+    def rider_name(self):
+        return self._rider_name
+    
+    @rider_name.setter
     def rider_name(self, name):
-        pass
+        if type(name) is str:
+            if len(name) > 0:
+                self._rider_name = name
+            else:
+                raise ValueError('Name must be more than 0 characters long.')
+        else:
+            TypeError('Name must be a string.')
+        
+
+
+
+
+
+
+
+
 
 
 
     from classes.event import Event
-    from classes.registration import Registration
-    from classes.skier import Skier
-    
+    # from classes.registration import Registration
+    # from classes.skier import Skier
+
 
 
