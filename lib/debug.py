2646ef0eedeaf62259c5bbd52064432a98e2e1f6
Hiroki Kato <hirokikato1@gmail.com>
2023-09-08 09:45:34 -0400

    incorporates TM  menu

--- lib/debug.py
+++ lib/debug.py
@@ -11,7 +11,85 @@ from classes import CONN, CURSOR
 if __name__ == '__main__':
     print('in debug file')
 
-    farhan = RidingTeam("farhan","lil Sebastian")
-    print(farhan.rider_name)
+    task = input( 'would you like to create and show all skiers. Enter : Y/N  ')
+if task == 'Y':
+    Skier.drop_table()    
+    Skier.create_table()
+    farhan = Skier('farhan')
+    hiro = Skier('Hiro')
+    farhan.save()
+    hiro.save()
+    tess = Skier.create("Tess")
+    Skier.get_all()
+    print("Intermission")
+    # Skier.find_by_name("Hiro")
+    # print("Intermission")
+    # Skier.find_by_id(1)
+    
+    #*******************************************
+    
+    RidingTeam.drop_table()    
+    RidingTeam.create_table()
+    r1 = RidingTeam('horse1', 'farhan')
+    r2 = RidingTeam('horse2','Hiro')
+    r1.save()
+    r2.save()
+    r3 = RidingTeam.create("Bullesye", "Tess")
+
+    #*******************************************
+
+    # print("Riding team get_all:")
+    # RidingTeam.get_all()
+    # print("Find by rider: hiro")
+    # RidingTeam.find_by_rider("Hiro")
+    # print("Find by horse: horse1")
+    # RidingTeam.find_by_horse("horse1")
+    # print("Intermission")
+    # RidingTeam.find_by_id(1)
+    
+    #*******************************************
+    
+    Event.drop_table()    
+    Event.create_table()
+    e1 = Event(1500, 'Dallas')
+    e2 = Event(2000,'Philly')
+    e1.save()
+    e2.save()
+    e3 = Event.create(5000, "NYC")
+
+    #*******************************************
+
+    # print("Getting all events: ")
+    # Event.get_all()
+    # print("Find event by location: NYC...")
+    # Event.find_by_location("NYC")
+    # print("Find event by ID: 1...")
+    # Event.find_by_id(1)
+    
+    #*******************************************
+
+    print("Creating Registrations")
+    Registration.drop_table()    
+    Registration.create_table()
+    test = farhan.create_registration(r2.id, e3.id)
+    reg1 = Registration(r1.id,farhan.id, e1.id)
+    reg2 = Registration(r2.id, hiro.id, e2.id)
+    reg3 = Registration(r1.id,farhan.id, e1.id)
+    reg4 = Registration(r2.id,farhan.id, e2.id)
+    reg1.save()
+    reg2.save()
+    reg3 = Registration.create(r3.id, tess.id, e3.id)
+
+    #*******************************************
+
+    print("Get all Registrations")
+    Registration.get_all()
+    print("Get all of farhans registrations:")
+    farhan.registrations()
+    print("Get all of farhans events:")
+    farhan.events()
+    print("Get all of hiro's registrations:")
+    hiro.registrations()
+
 
     ipdb.set_trace()
